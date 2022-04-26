d1294aa103fdde41436d7a45709c24f39d6dc94f
SCPNostalgia <32840908+SCPNostalgia@users.noreply.github.com>
2022-04-26 14:35:38 +0200

    XmlTest

--- TestScript.js
+++ TestScript.js
@@ -1,4 +1,14 @@
 window.alert('Hi, ich funktioniere!');
 window.alert('Es gibt ' + document.forms.length + ' Forms');
 
-var xmlDoc = document.implementation.createDocument(null, "books");
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+    var xmlDoc = xhttp.responseXML; //important to use responseXML here
+}
+xhttp.open("GET", "TestXml.xml", true);
+xhttp.send();}
+var node = xmlDoc.createElement("heyHo");
+var elements = xmlDoc.getElementsByTagName("root");
+elements[0].appendChild(node);
