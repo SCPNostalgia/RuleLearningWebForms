8fbdf57650b0362532f0175c01ede140532b0de6
SCPNostalgia <32840908+SCPNostalgia@users.noreply.github.com>
2022-04-28 11:50:34 +0200

    Update TestScript.js

--- TestScript.js
+++ TestScript.js
@@ -1,14 +1,23 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 window.alert('Hi, ich funktioniere!');
 window.alert('Es gibt ' + document.forms.length + ' Forms');
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-if (xhttp.readyState == 4 && xhttp.status == 200) {
+$(document).ready(function() {
+    var url = 'https://github.com/SCPNostalgia/RuleLearningWebForms/blob/main/TestXml.xml';
 
-    var xmlDoc = xhttp.responseXML; //important to use responseXML here
-}
-xhttp.open("GET", "https://github.com/SCPNostalgia/RuleLearningWebForms/blob/main/TestXml.xml", true);
-xhttp.send();}
-var node = xmlDoc.createElement("heyHo");
-var elements = xmlDoc.getElementsByTagName("root");
-elements[0].appendChild(node);
+    $.get(url, function(data) { 
+        $('#code').text(data);
+      }, 'text');
+    });
+window.alert(data);
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+// if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+//     var xmlDoc = xhttp.responseXML; //important to use responseXML here
+// }
+// xhttp.open("GET", "https://github.com/SCPNostalgia/RuleLearningWebForms/blob/main/TestXml.xml", true);
+// xhttp.send();}
+// var node = xmlDoc.createElement("heyHo");
+// var elements = xmlDoc.getElementsByTagName("root");
+// elements[0].appendChild(node);
